4245662f23dce3f47dc2b0196bf2c75bad7d460b
kabir.faye <fayekabir21@yahoo.com>
2023-12-25 12:13:58 +0100

    minor-modification

--- scripts/userdata.sh
+++ scripts/userdata.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Hardware requirements: AWS Linux 2 with mimum t2.micro type instance & port 8080(application port), 9100 (node-exporter port) should be allowed on the security groups
+# Hardware requirements: AWS Linux 2 with minimum t2.micro type instance & port 8080(application port), 9100 (node-exporter port) should be allowed on the security groups
 # setup for the ansible configuration
 sudo yum update –y
 sudo useradd ansadmin
